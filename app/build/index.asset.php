263b7f128e9e0d4fb88b76198767d0bcc60e1c39
Themehunk <themehunk@gmail.com>
2023-07-29 18:58:37 +0530

    all file build

--- app/build/index.asset.php
+++ app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-url'), 'version' => '19a080527cd08faba94e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-url'), 'version' => '7285a460a1cd00ce61f6');
